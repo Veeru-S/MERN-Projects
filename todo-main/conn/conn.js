e15a9aef26e8b0bfd9c8a586d34664e9358d036b
VEERU <142572918+Veeru-S@users.noreply.github.com>
2024-09-28 19:31:15 +0530

    conn.js

--- todo-main/conn/conn.js
+++ todo-main/conn/conn.js
@@ -4,7 +4,7 @@ const conn = async (req, res) => {
   try {
     await mongoose
       .connect(
-        ""
+        "mongodb+srv://veeru:<db_password>@cluster0.n5ozt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
       )
       .then(() => {
         console.log("Connected");
